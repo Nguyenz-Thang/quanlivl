e8804a6f34762180a5711fd51bbef6748d6e657a
tatthang <tatthang204@gmail.com>
2024-06-04 21:41:17 +0700

    uhu

--- login.php
+++ login.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
+<?php
 session_start();
 include 'config.php';
 
@@ -21,12 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($result->num_rows > 0) {
         $_SESSION['username'] = $username;
-        header("Location: dashboard.php");
+        header("Location: dashboard.html");
     } else {
         echo "Tên đăng nhập hoặc mật khẩu không đúng";
     }
 }
 ?>
-</body>
-
-</html>
